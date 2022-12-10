40ecdbaff81f7ba4677211989059fa921d088099
Deepak <deepakbond058@gmail.com>
2022-12-10 14:19:33 +0530

    env updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ connectToMongo();
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port=5000;
+const PORT=5000 ;
 const path = require("path");
 
 app.use(express.json());
@@ -18,6 +18,6 @@ app.use(express.static(path.join(__dirname,"./frontend/build")))
 app.get("*",(req,res)=>{
   res.sendFile(path.join(__dirname,"./frontend/build/index.html"));
 })
-app.listen(port, () => {
+app.listen(PORT || process.env.PORT, () => {
   console.log(`http://localhost:${port}`)
 })
