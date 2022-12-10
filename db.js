bbe6f90b2b285d028b58dfc7d74f33251fa490b8
Deepak <deepakbond058@gmail.com>
2022-12-10 22:04:48 +0530

    cssc

--- db.js
+++ db.js
@@ -2,9 +2,13 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 const path  = require('path');
 const mongoURI = process.env.REACT_APP_NOT_MONGO_URI;
-const connectToMongo = async()=>{
-    await mongoose.connect(mongoURI,()=>{
-        console.log("connected to mongopo");
-    })
-}
+const connectToMongo= mongoose.connect(
+    process.env.MONGO_URL,
+    options,
+    (err) => {
+     if(err) console.log(err) 
+     else console.log("mongdb is connected");
+    }
+  );
+
 module.exports = connectToMongo;
