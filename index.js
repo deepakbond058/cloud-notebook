ab6d410ec070611caa77d08a4f87e34c06d88b0a
Deepak <deepakbond058@gmail.com>
2022-12-10 14:25:47 +0530

    wd

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ app.get("*",(req,res)=>{
   res.sendFile(path.join(__dirname,"./frontend/build/index.html"));
 })
 app.listen(PORT || process.env.PORT, () => {
-  console.log(`http://localhost:${port}`)
+  console.log(`http://localhost:${PORT}`)
 })
